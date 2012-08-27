12bf554338faf32ccea804cdd17c393e2168e313
Benny Lichtner <bennlich@gmail.com>
2012-08-26 20:19:47 -0600

    Only loads firebase script if appropriate span elements are found on the page.

--- modules/ext.Firebase.js
+++ modules/ext.Firebase.js
@@ -1,10 +1,13 @@
 $(document).ready( function() {
-	$.getScript("http://static.firebase.com/v0/firebase.js", function() {
-		$("span.firebase").each(function(idx) {
-			var self = this;
-			new Firebase($(this).attr("id")).on("value", function(snapshot) {
-				$(self).text(snapshot.val());
+	var firebaseElements = $("span.firebase");
+	if (firebaseElements.length > 0) {
+		$.getScript("http://static.firebase.com/v0/firebase.js", function() {
+			firebaseElements.each(function(idx) {
+				var self = this;
+				new Firebase($(this).attr("id")).on("value", function(snapshot) {
+					$(self).text(snapshot.val());
+				});
 			});
 		});
-	});
+	}
 });
